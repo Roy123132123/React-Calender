f6202f14e6617cd0111403716e91e02473f6f969
royboot <rgrizell467@gmail.com>
2023-12-24 15:29:49 +0100

    Comments&Readme
    Added comments to the code and retyped the Readme

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a nice looking calender website made using the react framework and javascripts own Date api.
--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import React, { useEffect, useState } from 'react';
 
 
 export default function App() {
+//define all use state cases
 const [Longday, setLongDay] = useState('');
 const [LongMonth, setLongMonth] = useState('');
 const [dayNum, setDayNum] = useState('');
 const [year,setYear]= useState('');
+
 useEffect(() => {
   const date = new Date();
+  //get the long versions of the month and weekday instead of numbers
   const formattedDay = date.toLocaleDateString("en",{weekday:"long"});
   const formattedMonth = date.toLocaleDateString("en", {month:"long"});
+  //get the number of the day and the number of the current year
   const daynum = date.getDate();
   const Year = date.getFullYear();
+  //set all appropriate values
   setYear(Year);
   setDayNum(daynum);
   setLongDay(formattedDay);
